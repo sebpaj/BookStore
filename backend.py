47baa0f4aff62de6b6c3b78f6778ae43fb9ed4bd
spyddy93 <sseba07@gmail.com>
2018-06-04 19:28:40 +0200

    adding functions: insert to database and select all data from database

--- backend.py
+++ backend.py
@@ -7,4 +7,21 @@ def connect():
     conn.commit()
     conn.close()
 
+def insert(title,author,year,isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title,author,year,isbn)) #null because primary key is autoincrementing
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
 connect()
+insert("Wiedzmin","Sapko",200,123131)
+print(view())
