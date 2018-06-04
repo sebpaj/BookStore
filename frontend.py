1516585ec319cae8cb9872fc2d7df6a4deddf3a5
spyddy93 <sseba07@gmail.com>
2018-06-04 17:53:12 +0200

    adding entry fields

--- frontend.py
+++ frontend.py
@@ -27,4 +27,20 @@ l3.grid(row=1,column=0)
 l3 = Label(window,text="ISBN")
 l3.grid(row=1,column=2)
 
+titleText = StringVar()
+e1=Entry(window,textvariable=titleText)
+e1.grid(row=0,column=1)
+
+authorText = StringVar()
+e1=Entry(window,textvariable=authorText)
+e1.grid(row=0,column=3)
+
+yearText = StringVar()
+e1=Entry(window,textvariable=yearText)
+e1.grid(row=1,column=1)
+
+ISBNText = StringVar()
+e1=Entry(window,textvariable=ISBNText)
+e1.grid(row=1,column=3)
+
 window.mainloop()
