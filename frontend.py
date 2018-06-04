e1278a5108c5d769b86bc7d0b264c1db14871c58
spyddy93 <sseba07@gmail.com>
2018-06-04 18:37:55 +0200

    adding list, scrolbar and updateing entries

--- frontend.py
+++ frontend.py
@@ -32,15 +32,26 @@ e1=Entry(window,textvariable=titleText)
 e1.grid(row=0,column=1)
 
 authorText = StringVar()
-e1=Entry(window,textvariable=authorText)
-e1.grid(row=0,column=3)
+e2=Entry(window,textvariable=authorText)
+e2.grid(row=0,column=3)
 
 yearText = StringVar()
-e1=Entry(window,textvariable=yearText)
-e1.grid(row=1,column=1)
+e3=Entry(window,textvariable=yearText)
+e3.grid(row=1,column=1)
 
 ISBNText = StringVar()
-e1=Entry(window,textvariable=ISBNText)
-e1.grid(row=1,column=3)
+e4=Entry(window,textvariable=ISBNText)
+e4.grid(row=1,column=3)
+
+list1 = Listbox(window,height=6,width=35)
+list1.grid(row=2,column=0,rowspan=6,columnspan=2)
+
+sb1 = Scrollbar(window)
+sb1.grid(row=2,column=2)
+
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
+
 
 window.mainloop()
